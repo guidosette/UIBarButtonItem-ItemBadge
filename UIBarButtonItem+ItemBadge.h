f3177a70be6f21717ae37eb4bafe60b21edd3a4c
Guido Fanfani <guido.fanfani7@gmail.com>
2017-09-29 14:21:25 +0200

    Update UIBarButtonItem+ItemBadge.h

--- UIBarButtonItem+ItemBadge.h
+++ UIBarButtonItem+ItemBadge.h
@@ -2,7 +2,7 @@
 //  UIBarButtonItem+ItemBadge.h
 //  Fanfa
 //
-//  Created by Guido Fanfani on 21/09/17.
+//  Created by Guido Fanfani on 23/09/17.
 //  Copyright © 2017 Fanfa. All rights reserved.
 //
 
